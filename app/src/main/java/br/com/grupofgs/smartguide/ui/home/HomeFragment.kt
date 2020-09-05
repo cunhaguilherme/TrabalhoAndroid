dd1e953bc74a2fe71967fba7a582bbde56a204b8
fabio.asnis <asnisapps@gmail.com>
2020-09-05 10:42:16 -0300

    MapFragment created
    Created with navigation from HomeFragment button

--- app/src/main/java/br/com/grupofgs/smartguide/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/grupofgs/smartguide/ui/home/HomeFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.NavHostFragment
 import br.com.grupofgs.smartguide.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
+    private lateinit var btMap: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,24 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpView(view)
+    }
+
+    private fun setUpView(view: View) {
+        btMap = view.findViewById(R.id.btMap)
+
+        btMap.setOnClickListener {
+            NavHostFragment.findNavController(this)
+                    .navigate(
+                            R.id.action_homeFragment_to_mapFragment,
+                            null,
+                            null
+                    )
+        }
     }
 
 }
