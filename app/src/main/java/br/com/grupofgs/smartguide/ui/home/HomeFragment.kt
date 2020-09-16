16dce1ba55e0d268f93242d7481ac4f9a27d9f4d
fwfcunha <39678921+fwfcunha@users.noreply.github.com>
2020-09-16 00:36:49 -0300

    Update HomeFragment.kt

--- app/src/main/java/br/com/grupofgs/smartguide/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/grupofgs/smartguide/ui/home/HomeFragment.kt
@@ -1,7 +1,6 @@
 package br.com.grupofgs.smartguide.ui.home
 
 import android.os.Bundle
-import android.transition.TransitionInflater
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.transition.TransitionInflater
