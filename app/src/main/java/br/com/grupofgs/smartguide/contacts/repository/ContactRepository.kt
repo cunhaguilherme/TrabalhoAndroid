a086b9412a371676e5041f5bff4cbc6d98030ac1
fwfcunha <filipewalter1@gmail.com>
2020-09-19 13:41:54 -0300

    ROOM AND SQLITE
    Creating contact area

---
+++ app/src/main/java/br/com/grupofgs/smartguide/contacts/repository/ContactRepository.kt
@@ -0,0 +1,19 @@
+package br.com.grupofgs.smartguide.contacts.repository
+
+import androidx.lifecycle.LiveData
+import br.com.grupofgs.smartguide.contacts.dao.ContactDao
+import br.com.grupofgs.smartguide.contacts.data.Contact
+
+
+// Declares the DAO as a private property in the constructor. Pass in the DAO
+// instead of the whole database, because you only need access to the DAO
+class ContactRepository(private val contactDao: ContactDao) {
+
+    // Room executes all queries on a separate thread.
+    // Observed LiveData will notify the observer when the data has changed.
+    val allWords: LiveData<List<Contact>> = contactDao.getAlphabetizedWords()
+
+    suspend fun insert(word: Contact) {
+        contactDao.insert(word)
+    }
+}
